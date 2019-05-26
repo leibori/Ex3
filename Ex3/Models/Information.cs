f3b19c9178789b29d33c00a46eeab56bc647c0b5
ori leibovitz <ori.leibo@gmail.com>
2019-05-26 14:33:48 +0300

    add models files

---
+++ Ex3/Models/Information.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+namespace Ex3.Models
+{
+    public class Information
+    {
+        public int Lat { get; set; }
+        public int Lon { get; set; }
+        public int Hight { get; set; }
+        public int Dircation { get; set; }
+        public int Speed { get; set; }
+
+        public void ToXml(XmlWriter writer)
+        {
+            writer.WriteStartElement("Information");
+            writer.WriteElementString("Lat", this.Lat.ToString());
+            writer.WriteElementString("Lat", this.Lon.ToString());
+            writer.WriteElementString("Hight", this.Hight.ToString());
+            writer.WriteElementString("Dircation", this.Dircation.ToString());
+            writer.WriteElementString("Speed", this.Speed.ToString());
+
+        
+            writer.WriteEndElement();
+        }
+
+    }
+}
