2d39801afefd995162b4fe16d2cddd1f008bd3e1
Amit Ashkenazy <amit471@gmail.com>
2019-05-26 15:22:52 +0300

    Added HttpGet functions to MapController.

--- Ex3/Controllers/MapController.cs
+++ Ex3/Controllers/MapController.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ex3.Models
 
 namespace Ex3.Controllers
 {
@@ -11,6 +12,51 @@ namespace Ex3.Controllers
         // GET: Map
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public bool isIp(string param)
+        {
+            string[] data = param.split('.');
+            if (data.Length != 4) { return false; }
+
+             int parsedInt = 0;
+            for (int index = 0; index < 4; ++index)
+            {
+                if (int.TryParse(data[index], out parsedInt))
+                {
+                    if (parsedInt < 0 || 255 < parsedInt) { return false; }
+                }
+                else { return false; }
+            }
+
+            return true;
+        }
+
+        [HttpGet]
+        public ActionResult display(string param1, int param2)
+        {
+            if (isIp(param1))
+            {
+                InfoModel.Instance.ip = ip;
+                InfoModel.Instance.port = port.ToString();
+            }
+            else
+            {
+                InfoModel.fileName = param1
+                InfoModel.timesPerSec = param2
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult display(string ip, int port, int timesPerSec)
+        {
+            InfoModel.Instance.ip = ip;
+            InfoModel.Instance.port = port.ToString();
+            InfoModel.Instance.time = timesPerSec;
+
             return View();
         }
     }
