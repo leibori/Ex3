2cf831f8a8f7f28725c6776a85ca75134547a24b
ori leibovitz <ori.leibo@gmail.com>
2019-05-26 15:24:15 +0300

    update InfoModel

--- Ex3/Models/InfoModel.cs
+++ Ex3/Models/InfoModel.cs
@@ -7,5 +7,28 @@ namespace Ex3.Models
 {
     public class InfoModel
     {
+        private static InfoModel s_instace = null;
+
+        public static InfoModel Instance
+        {
+            get
+            {
+                if (s_instace == null)
+                {
+                    s_instace = new InfoModel();
+                }
+                return s_instace;
+            }
+        }
+        public Information Information { get; private set; }
+        public string file { get; set; }
+        public string ip { get; set; }
+        public string port { get; set; }
+        public int time { get; set; }
+
+        public InfoModel()
+        {
+            Information = new Information();
+        }
     }
 }
