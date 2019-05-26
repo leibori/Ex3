a59be5ddf207a656ee2dc6c22964296e641a3374
ori leibovitz <ori.leibo@gmail.com>
2019-05-26 13:39:46 +0300

    adding Map controller

--- Ex3/App_Start/RouteConfig.cs
+++ Ex3/App_Start/RouteConfig.cs
@@ -14,18 +14,18 @@ namespace Ex3
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute("display", "display/{ip}/{port}/{timesPerSec}",
-            defaults: new { controller = "First", action = "display" });
+            defaults: new { controller = "Map", action = "display" });
 
             routes.MapRoute("display", "display/{param1}/{param2}}",
-            defaults: new { controller = "First", action = "display" });
+            defaults: new { controller = "Map", action = "display" });
 
             routes.MapRoute("save", "save/{port}/{ip}/{timesPerSec}/{time}/{fileName}",
-            defaults: new { controller = "First", action = "save" });
+            defaults: new { controller = "Map", action = "save" });
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Map", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
