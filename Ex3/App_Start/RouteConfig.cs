f861910dc450ffb2e11626d2f9c7e56dee1a6812
Amit Ashkenazy <amit471@gmail.com>
2019-05-30 22:12:20 +0300

    Split the display to two different view files "display1" and "display2".

--- Ex3/App_Start/RouteConfig.cs
+++ Ex3/App_Start/RouteConfig.cs
@@ -13,8 +13,11 @@ namespace Ex3
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute("display", "display/{param1}/{param2}/{param3}",
-            defaults: new { controller = "Map", action = "display", param3 = UrlParameter.Optional });
+            routes.MapRoute("display1", "display/{ip}/{port}/{timesPerSec}",
+            defaults: new { controller = "Map", action = "display1" });
+
+            routes.MapRoute("display2", "display/{param1}/{param2}",
+            defaults: new { controller = "Map", action = "display2" });
 
             routes.MapRoute("save", "save/{ip}/{port}/{timesPerSec}/{time}/{fileName}",
             defaults: new { controller = "Map", action = "save" });
